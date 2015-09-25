a876547f8e1a37140cf6a7b0dac87c0068e90dac
Kristian Rosenvold <krosenvold@apache.org>
2015-09-25 15:46:29 +0200

    Moar test cases

--- src/test/java/org/apache/maven/model/immutable/ImmutableModelBuilderTest.java
+++ src/test/java/org/apache/maven/model/immutable/ImmutableModelBuilderTest.java
@@ -2,6 +2,7 @@ package org.apache.maven.model.immutable;
 
 import org.apache.maven.model.InputSource;
 import org.apache.maven.model.Model;
+import org.apache.maven.model.Parent;
 import org.apache.maven.model.immutable.model.Plugin;
 import org.apache.maven.model.immutable.model.Project;
 import org.apache.maven.model.io.xpp3.MavenXpp3ReaderEx;
@@ -13,6 +14,7 @@ import javax.xml.stream.XMLStreamException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
+import java.util.Properties;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
@@ -20,7 +22,9 @@ import static org.junit.Assert.assertNotNull;
 public class ImmutableModelBuilderTest
 {
 
-    private final URL simpleTest = this.getClass().getResource( "/immutable/simpletest.xml" );
+    private final URL simpleTest = this.getClass()
+                                       .getResource( "/immutable/simpletest.xml" );
+
     @Test
     public void testBuildModel()
         throws Exception
@@ -51,7 +55,7 @@ public class ImmutableModelBuilderTest
     public void malformed_plugins_ex()
         throws Exception
     {
-        readModel( "/immutable/simple-malformed-plugins.xml" );
+        readModelXpp3( "/immutable/simple-malformed-plugins.xml" );
 
     }
 
@@ -130,6 +134,83 @@ public class ImmutableModelBuilderTest
         assertNotNull( project );
     }
 
+    @Test
+    public void assertCorePom()
+        throws IOException, XmlPullParserException
+    {
+
+        Model model = readModelXpp3( "/immutable/maven-core.xml" );
+        assertCoreModel( model );
+    }
+
+    private void assertCoreModel( Model model )
+    {
+        assertEquals( "maven", model.getArtifactId());
+        assertEquals( "zz", model.getGroupId());
+        assertEquals( "3.3.7-SNAPSHOT", model.getVersion());
+        assertEquals( "pom", model.getPackaging());
+        assertEquals("Apache Maven Not", model.getName());
+        assertEquals("DescrA", model.getDescription());
+        assertEquals("http://maven.apache.org/ref/${project.version}/", model.getUrl());
+        assertEquals("2001", model.getInceptionYear());
+        assertParent( model.getParent() );
+        assertProperties( model );
+        assertModules( model );
+        assertScm( model );
+        assertIssueManagement( model );
+        assertCiManagement( model);
+    }
+
+    private void assertIssueManagement( Model model )
+    {
+        assertEquals("jira", model.getIssueManagement().getSystem());
+        assertEquals("https://issues.apache.org/jira/browse/MNG", model.getIssueManagement().getUrl());
+    }
+
+    private void assertDistributionManagement( Model model )
+    {
+        assertEquals("http://maven.apache.org/download.html", model.getDistributionManagement().getDownloadUrl());
+        assertEquals("https://issues.apache.org/jira/browse/MNG", model.getIssueManagement().getUrl());
+    }
+
+    private void assertCiManagement( Model model )
+    {
+        assertEquals("Jenkins", model.getCiManagement().getSystem());
+        assertEquals("https://builds.apache.org/job/maven-3.x/", model.getCiManagement().getUrl());
+    }
+
+    private void assertScm( Model model )
+    {
+        assertEquals( "scm:git:https://git-wip-us.apache.org/repos/asf/maven.git", model.getScm().getConnection());
+        assertEquals( "scm:git!:https://git-wip-us.apache.org/repos/asf/maven.git", model.getScm().getDeveloperConnection());
+        assertEquals( "https://github.com/apache/maven/tree/${project.scm.tag}", model.getScm().getUrl());
+        assertEquals( "master", model.getScm().getTag());
+    }
+
+    private void assertModules( Model model )
+    {
+        assertEquals( 13, model.getModules().size());
+        assertEquals( "maven-plugin-api", model.getModules().get(0));
+        assertEquals( "apache-maven", model.getModules().get(12));
+    }
+
+    private void assertProperties( Model model )
+    {
+        final Properties properties = model.getProperties();
+        assertEquals( 25, properties.size());
+        assertEquals( "1.8", properties.get("maven.compiler.source")); // wishful thinking
+        assertEquals( "2.5.2", properties.get("classWorldsVersion"));
+        assertEquals( "**/package-info.java", properties.get("checkstyle.excludes"));
+    }
+
+    private void assertParent( Parent parent )
+    {
+        assertEquals( "org.apache.maven", parent.getGroupId() );
+        assertEquals( "maven-parent", parent.getArtifactId() );
+        assertEquals( "26", parent.getVersion() );
+        assertEquals( "../pom/maven/pom.xml", parent.getRelativePath() );
+    }
+
     // TOOD: Test Case sensitvity of tagnames ?
     // TODO: Test more malformed poms
 
@@ -142,7 +223,7 @@ public class ImmutableModelBuilderTest
         hc = 0;
         for ( int i = 0; i < 1000; i++ )
         {
-            Model model = readModel( simpleTest );
+            Model model = readModelXpp3( simpleTest );
             hc += model.hashCode();
         }
         System.out.println( "xpp3 = " + ( System.currentTimeMillis() - start ) + "hc = " + hc );
@@ -164,21 +245,22 @@ public class ImmutableModelBuilderTest
     }
 
 
-    private Model readModel( String name )
+    private Model readModelXpp3( String name )
         throws IOException, XmlPullParserException
     {
         MavenXpp3ReaderEx org = new MavenXpp3ReaderEx();
-        try ( InputStream resourceAsStream = this.getClass().getResourceAsStream( name ) )
+        try (InputStream resourceAsStream = this.getClass()
+                                                .getResourceAsStream( name ))
         {
             return org.read( resourceAsStream, true, new InputSource() );
         }
     }
 
-    private Model readModel( URL name )
+    private Model readModelXpp3( URL name )
         throws IOException, XmlPullParserException
     {
         MavenXpp3ReaderEx org = new MavenXpp3ReaderEx();
-        try ( InputStream resourceAsStream = name.openStream() )
+        try (InputStream resourceAsStream = name.openStream())
         {
             return org.read( resourceAsStream, true, new InputSource() );
         }
